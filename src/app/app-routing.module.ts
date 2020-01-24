73e88160894958121e60c11f55a7f71ede540e74
dnl092 <drogalsky14@my.whitworth.edu>
2020-01-24 14:32:00 -0800

    changed accent colors throughout and basically finished the login stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: FrontPageComponent },
-  { path: 'login', component: LoginComponent}
+  { path: 'gmlogin', component: LoginComponent}
 ];
 
 @NgModule({
